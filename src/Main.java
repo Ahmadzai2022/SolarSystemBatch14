2e4caba86df9f3d981da7f5d209601bff5a5cf09
Ahmadzai2022 <h.ahmadzai2021@gmail.com>
2022-12-21 15:53:15 -0500

    The structure of the solar system

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("The Basic Structure is Ready Plase write down your code in this "
-                + "project");
+
     }
 }
---
+++ src/SolarSystem.java
@@ -0,0 +1,13 @@
+public class SolarSystem {
+    int sun;
+    int moon;
+    int planets;
+
+    public static void main(String[] args) {
+        SolarSystem Jinny = new SolarSystem();
+        Jinny.sun=1;
+        Jinny.moon=2;
+        Jinny.planets=3;
+
+    }
+}
